365a3a5ad4677b32d11714cb810aa052f3a5e2fe
dhyeykakadiya71-dotcom <dhyeykakadiya71@gmail.com>
2026-04-04 19:40:18 +0530

    Update and rename c u later.cpp to p_1_1.cpp

---
+++ p_1_1.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+	float c;
+	
+	printf("enter the c: ");
+	scanf("%f", &c);
+
+	printf("\nyou farenheit: %.1f|", (9.0/5.0*c)+32);
+}
+/*
+		output:
+		enter the c: 38
+
+		you farenheit: 100.4
+
+*/
