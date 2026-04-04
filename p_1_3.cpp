49b90f16f8f57f1edb4dd567bb4b83ec79f7faf1
dhyeykakadiya71-dotcom <dhyeykakadiya71@gmail.com>
2026-04-04 19:36:33 +0530

    Update and rename ok bye.cpp to p_1_3.cpp

---
+++ p_1_3.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+	const int a=65;
+	const int b=45;
+	const int total=180;
+
+	printf("\nYour third angel c: %d", total-(a+b));
+/*
+		output:
+		Your third angel c: 70 
+*/
